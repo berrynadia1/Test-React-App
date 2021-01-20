1346e49f90d70d046aadee0a0db4b9bbceae1506
berrynadia1 <nadiaoberry@gmail.com>
2021-01-20 13:55:35 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import List from './Components/List'
 
-function App() {
+class App extends React.Component {
+  //tracking user input: using a form or an input field
+  //onChange is probably needed
+  //onSubmit is definitely needed!
+  //I'll probably need a preventDefault
+  //State just to keep my information the same and only change when needed to change
+
+constructor(props){
+  super(props)
+  this.state={
+    noteText:'',
+    notes:[]
+  }
+
+}
+
+onChange = (event) =>{
+  this.setState({
+    noteText:event.target.value
+  })
+}
+onSubmit = (event) =>{
+  event.preventDefault()
+  this.setState({
+    noteText:'',
+    notes:[...this.state.notes, this.state.noteText]
+  })
+  console.log(this.state.notes)
+}
+
+
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div className="header">
+          <div className ="wrapper">
+          <img src = "img/r2h-icon.png" />
+          <p className="title">Road To Hire</p>
+          </div>
+        </div>
+        <div className="app">
+          <div className="app-wrapper">
+            <p className="app-title">To-Do List</p>
+            <form className ="App" onSubmit = {this.onSubmit}>
+              <input value = {this.state.noteText} onChange = {this.onChange} placeholder="Type here to add a task" type="text" id="box" />
+              <br />
+            </form>
+            <br />
+            <List notes = {this.state.notes} />
+    </div>
+    </div>
     </div>
   );
 }
-
+}
 export default App;
